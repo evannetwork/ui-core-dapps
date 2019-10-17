1c138099b79797d134cbe60c8b96d6a5f06fda33
Daniel Maaß <daniel.maass@evan.team>
2019-10-17 16:28:20 +0200

    move translation to evancore.vue.libs
    -[CORE-529]

--- dapps/profile.vue/src/i18n/de.ts
+++ dapps/profile.vue/src/i18n/de.ts
@@ -163,16 +163,7 @@ const i18n: any = {
     "sharings": {
       "desc": "Nachfolgend finden Sie alle Kontakte, mit denen Sie diese Profilinformationen derzeit teilen. Klicken Sie auf ein Profil, um die Berechtigungen anzupassen.", 
       "title": "Freigaben für deine Profildaten",
-      "no-data": "Bisher wurden keine Profildaten freigegeben",
-      "permission": {
-        "type": {
-          "fullAccess": "Vollzugriff",
-          "fullRead": "komplette Leserechte",
-          "readWrite": "individuelle Schreib- und Leserechte",
-          "read": "individuelle Leserechte",
-          "none": "keine Berechtigungen"
-        }
-      },
+      "no-data": "Bisher wurden keine Profildaten freigegeben"
     },
     "type": {
       "change": "Ändern",
